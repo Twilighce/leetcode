97a0667632a1cca5b2764cc713f42a42a34962af
Twilighce <rbing@mail.ustc.edu.cn>
2018-01-23 13:22:06 +0800

    180123

---
+++ Valid_Palindrome.java
@@ -0,0 +1,44 @@
+/*
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+we define empty string as valid palindrome.
+*/
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+        	return true;
+        }
+        int head = 0, tail = s.length() - 1;
+        char cHead, cTail;
+        while(head <= tail) {
+        	cHead = s.charAt(head);
+        	cTail = s.charAt(tail);
+        	if (!Character.isLetterOrDigit(cHead)) {
+        		head++;
+        	} else if(!Character.isLetterOrDigit(cTail)) {
+        		tail--;
+        	} else {
+        		if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+        			return false;
+        		}
+        		head++;
+        		tail--;
+        	}
+        }
+        
+        return true;
+    }
+}
+
+
+/*
+public class Solution {
+    public boolean isPalindrome(String s) {
+        String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String rev = new StringBuffer(actual).reverse().toString();
+        return actual.equals(rev);
+    }
+}
+
+*/
